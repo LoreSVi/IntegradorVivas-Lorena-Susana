c820b3784e50757c449d77fcf407d7c866034cb2
LoreSVi <loresuvivas@gmail.com>
2023-01-12 17:01:41 -0300

    cambios en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardSoftSkillsComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
