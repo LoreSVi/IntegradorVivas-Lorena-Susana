68d73b1d22be96e8172403c17a696dd69a15945b
LoreSVi <loresuvivas@gmail.com>
2023-03-26 04:58:28 -0300

    model y service, y modificaciones varias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { IndexComponent } from './components/index/index.component';
 import { Error404Component } from './components/error404/error404.component';
 import { LoginComponent } from './components/login/login.component';
 
+
 const routes: Routes = [
  {path: '',component: IndexComponent},
- {path: 'login', component: LoginComponent},
- {path: '**', component: Error404Component}
+ {path:'login', component: LoginComponent},
+ {path: '**', component: Error404Component},
 ];
 
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { BannerComponent } from './components/banner/banner.component';
@@ -14,7 +15,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { FooterComponent } from './components/footer/footer.component';
 import { Error404Component } from './components/error404/error404.component';
 import { IndexComponent } from './components/index/index.component';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './components/login/login.component';
+
 
 
 
@@ -33,12 +36,19 @@ import { LoginComponent } from './components/login/login.component';
     IndexComponent,
     LoginComponent,
      ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    BrowserAnimationsModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
